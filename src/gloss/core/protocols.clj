594fcb9e9022f96715864d03c5a7383e21418dc2
Luo Tianj <lt@basecity.com>
2020-06-04 20:20:58 +0800

    modernize protocols

--- src/gloss/core/protocols.clj
+++ src/gloss/core/protocols.clj
@@ -7,19 +7,18 @@
 ;;   You must not remove this notice, or any other, from this software.
 
 (ns ^{:skip-wiki true}
-  gloss.core.protocols
-  (:use
-	[potemkin]
-    [gloss.core formats])
+ gloss.core.protocols
+  (:require
+   [gloss.core.formats :as format :refer [to-buf-seq]])
   (:import
-    [java.nio Buffer ByteBuffer]
-    [gloss.data.bytes.core SingleBufferSequence]))
+   [java.nio Buffer ByteBuffer]
+   [gloss.data.bytes.core SingleBufferSequence]))
 
 ;;;
 
-(defprotocol+ Reader
+(defprotocol Reader
   (read-bytes [this buf-seq]
-     "Attempt to decode the given sequence of byte buffers. Returns a 3-tuple [success x remainder].
+    "Attempt to decode the given sequence of byte buffers. Returns a 3-tuple [success x remainder].
      If success is truthy, then x is the decoded value and remainder is whatever is left after
      decoding x.
 
@@ -33,7 +32,7 @@
      If, rather than being incomplete, the bytes to be read are faulty in some way, an exception
      will be thrown."))
 
-(defprotocol+ Writer
+(defprotocol Writer
   (sizeof [this]
     "Returns the number of bytes this codec will encode to, or nil if it is value-dependent.")
   (write-bytes [this buf val]))
@@ -56,22 +55,22 @@
 ;;;
 
 (defn frame-seq [reader buf-seq]
-  (loop [result [], buf-seq buf-seq]
+  (loop [result [] buf-seq buf-seq]
     (if (empty? buf-seq)
       result
       (let [[success x xs] (read-bytes reader (to-buf-seq buf-seq))]
-	(if success
-	  (recur (conj result x) xs)
-	  result)))))
+        (if success
+          (recur (conj result x) xs)
+          result)))))
 
 (defn compose-callback [codec callback]
   (reify
     Reader
     (read-bytes [this buf-seq]
       (let [[success x bytes] (read-bytes codec buf-seq)]
-	(if success
-	  (callback x bytes)
-	  [false (compose-callback x callback) (to-buf-seq bytes)])))
+        (if success
+          (callback x bytes)
+          [false (compose-callback x callback) (to-buf-seq bytes)])))
     Writer
     (sizeof [_]
       (sizeof codec))
@@ -83,15 +82,15 @@
     Reader
     (read-bytes [this buf-seq]
       (let [[success x bytes] (read-bytes a buf-seq)]
-	(if success
-	  (read-bytes
-	    (compose-callback
-	      b
-	      (fn [v remainder]
-		(assert (empty? remainder))
-		[true v bytes]))
-	    x)
-	  [false (compose-readers x b) (to-buf-seq bytes)])))
+        (if success
+          (read-bytes
+           (compose-callback
+            b
+            (fn [v remainder]
+              (assert (empty? remainder))
+              [true v bytes]))
+           x)
+          [false (compose-readers x b) (to-buf-seq bytes)])))
     Writer
     (sizeof [_]
       nil)
@@ -102,11 +101,11 @@
   (fn [buf-seq remainder]
     (loop [bytes (to-buf-seq buf-seq), vals []]
       (if (empty? bytes)
-	[true vals remainder]
-	(let [[success v b] (read-bytes codec bytes)]
-	  (when-not success
-	    (throw (Exception. "Cannot evenly divide bytes into sequence of frames.")))
-	  (recur b (conj vals v)))))))
+        [true vals remainder]
+        (let [[success v b] (read-bytes codec bytes)]
+          (when-not success
+            (throw (Exception. "Cannot evenly divide bytes into sequence of frames.")))
+          (recur b (conj vals v)))))))
 
 ;;;
 
